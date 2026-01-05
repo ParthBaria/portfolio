00bc664dd7856cc2759f6de3a316fc931d59dc98
intellysis-gaurav <Gaurav@intellysisdigital.com>
2026-01-05 21:51:27 +0530

    implementing lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Learning from "./pages/Learning";
+import { lazy, Suspense } from "react";
+const Home = lazy(() => import("./pages/Home"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Learning = lazy(() => import("./pages/Learning"));
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="projects" element={<Projects />} />
-          <Route path="projects/:pn" element={<Projects />} />
-          <Route path="learning" element={<Learning />} />
-          <Route path="projects/:pn" element={<Projects />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>loading</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="projects/:pn" element={<Projects />} />
+            <Route path="learning" element={<Learning />} />
+            <Route path="projects/:pn" element={<Projects />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }
